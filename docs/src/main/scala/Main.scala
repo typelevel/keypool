580690e72a7623c5426477d73db0872a37b0bb8a
Chris Davenport <chris@christopherdavenport.tech>
2019-04-11 08:09:20 -0700

    Change App Bounds

--- docs/src/main/scala/Main.scala
+++ docs/src/main/scala/Main.scala
@@ -4,7 +4,7 @@ package io.chrisdavenport.keypool
 import cats.implicits._
 import cats.effect._
 import cats.effect.concurrent._
-// import scala.concurrent.duration._
+import scala.concurrent.duration._
 
 object Main extends IOApp {
 
@@ -15,8 +15,8 @@ object Main extends IOApp {
         {_: Unit => Ref[IO].of(0)},
         {(_: Unit, r: Ref[IO, Int]) => r.get.flatMap{i => IO(println(s"Shutdown with $i"))}},
         Reuse,
-        Long.MaxValue,
-        3,
+        0L,
+        2,
         3,
         _ => IO.unit
     //     kpCreate: Key => F[Rezource],
@@ -26,10 +26,10 @@ object Main extends IOApp {
     // kpMaxPerKey: Int,
     // kpMaxTotal: Int,
     // onReaperException: Throwable => F[Unit]
-      ).use{kp => 
-        List.fill(100)(()).parTraverse( _ => 
-          kp.take(()).use( m => 
-            m.resource.modify(i => (i+1, i+1)).flatMap(i =>  kp.state.flatMap(state => IO(println(s"Got: $i - State: $state"))))
+      ).use{kp =>
+        List.fill(100)(()).parTraverse( _ =>
+          kp.take(()).use( m =>
+            m.resource.modify(i => (i+1, i+1)).flatMap(i =>  kp.state.flatMap(state => IO(println(s"Got: $i - State: $state")))) >> Timer[IO].sleep(1.nano)
           )
         )
       }.as(ExitCode.Success)
