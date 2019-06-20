dbd9b0d9a87237021da3c2899b8f64ae8945829a
Chris Davenport <chris@christopherdavenport.tech>
2019-06-20 14:20:50 -0700

    Switch naming convention to value

--- core/src/main/scala/io/chrisdavenport/keypool/Managed.scala
+++ core/src/main/scala/io/chrisdavenport/keypool/Managed.scala
@@ -11,8 +11,8 @@ import cats.effect.concurrent.Ref
  * scope will dictate whether it is shutdown or returned
  * to the pool.
  */
-final class Managed[F[_], Rezource] private[keypool] (
-  val resource: Rezource,
+final class Managed[F[_], A] private[keypool] (
+  val value: A,
   val isReused: Boolean,
   val canBeReused: Ref[F, Reusable]
 )
@@ -20,7 +20,7 @@ final class Managed[F[_], Rezource] private[keypool] (
 object Managed {
   implicit def managedFunctor[F[_]]: Functor[Managed[F, ?]] = new Functor[Managed[F, ?]]{
     def map[A, B](fa: Managed[F,A])(f: A => B): Managed[F,B] = new Managed[F, B](
-      f(fa.resource),
+      f(fa.value),
       fa.isReused,
       fa.canBeReused
     )
